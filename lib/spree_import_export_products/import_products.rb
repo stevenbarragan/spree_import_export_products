0b67e143f56a621216500335f031b0f303df5c41
Steven Barragan <me@steven.mx>
2013-10-16 00:41:22 -0500

    WIP create taxon and taxonomies on the import * one test case still failing

--- lib/spree_import_export_products/import_products.rb
+++ lib/spree_import_export_products/import_products.rb
@@ -1,38 +1,42 @@
-require 'CSV'
+require 'csv'
 
 module SpreeImportExportProducts
   class ImportProducts
-    def initilize
-      @associations_keys = [:taxons]
+    def initialize
+      @association_keys = [:taxons]
+      @products = []
+      @associations = []
     end
 
     def parse_file(file)
       csv = CSV.read(file)
       head = csv.shift.map{ |key| key.to_sym }
-      @product = []
-      @associations = []
+
       csv.each do |row|
         hash = Hash[head.zip(row)]
-        @products << hash.select{ |key,value| !@associations_keys.include?(key) }
-        @associations << hash.select{ |key,value| @associations_keys.include?(key) }
+        @products << hash.select{ |key,value| !@association_keys.include?(key) }
+        @associations << hash.select{ |key,value| @association_keys.include?(key) }
       end
     end
 
     def load(file)
       parse_file(file)
-      products = Spree::Product.create(@products)
-      products.each_with_index do |index,product|
+      Spree::Product.create(@products)
+    end
+
+    def load_associations(products)
+      products.each_with_index do |product,index|
         @associations[index].each do |association|
-          association.each do |key,value|
-            parent, value = value.split ':'
-            if value
-              taxonomy_id = Spree::Taxonomy.find_or_create(parent)
-              product.taxons << Spree::Taxon.create taxonomy_id: taxonomy_id, name: value
-            else
-              product.update key,elements[0]
-            end
-          end
+          send("create_#{association.keys.first}(product,association.values.first)")
         end
+      end
+    end
+
+    def create_taxon(products, association)
+      products.each do |product|
+        taxonomy_name,taxon_name = association.split(':')
+        taxonomy = Spree::Taxonomy.find_by_name(taxonomy_name) || Spree::Taxonomy.create(name: taxonomy_name)
+        product.taxons << Spree::Taxon.create(name: taxon_name, taxonomy_id: taxonomy.id)
       end
     end
   end
--- spec/lib/spree_import_export_products/import_products_spec.rb
+++ spec/lib/spree_import_export_products/import_products_spec.rb
@@ -4,8 +4,9 @@ module SpreeImportExportProducts
   describe ImportProducts do
     describe '#parse_file' do
       it 'parse a csv file' do
-        file_parsed = subject.parse_file(Rails.root.join('spec','test.csv'))
-        expect(file_parsed).to eq [{ name: 'Test name', price: '20', shipping_category_id: '1'}]
+        subject.parse_file(Rails.root.join('spec','test.csv'))
+        expect(subject.instance_variable_get(:@products)).to eq [{ name: 'Test name', price: '20', shipping_category_id: '1'}]
+        expect(subject.instance_variable_get(:@associations)).to eq [{ taxons: 'brand:test_brand'}]
       end
     end
 
@@ -14,6 +15,22 @@ module SpreeImportExportProducts
         expect do
           subject.load(Rails.root.join('spec','test.csv'))
         end.to change{Spree::Product.count}.from(0).to(1)
+      end
+    end
+
+    describe '#create_taxon' do
+      let(:product){ FactoryGirl.create :product }
+
+      before do
+        subject.create_taxon([product], 'brand:test_brand')
+      end
+
+      it 'creates a taxonomy' do
+        expect(Spree::Taxonomy.count).to eq 1
+      end
+
+      it 'creates a taxon' do
+        expect(Spree::Taxon.count).to eq 1
       end
     end
   end
